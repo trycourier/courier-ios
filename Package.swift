eddaaec77a5acf7522d8bf7ad1b2ce522cbc49cc
Michael Miller <mike@Michaels-MacBook-Pro.local>
2022-07-22 10:03:59 -0400

    FCM example cleanup

--- Package.swift
+++ Package.swift
@@ -11,10 +11,7 @@ let package = Package(
             targets: ["Courier"]
         ),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
         .target(
             name: "Courier",
