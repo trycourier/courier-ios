fbfe7b4249276b530cfec24c2118a7507d765753
Michael Miller <mike@Michaels-MacBook-Pro.local>
2022-08-03 11:18:56 -0400

    Custom Extension Tweaks

--- Sources/Courier/Service/CourierNotificationServiceExtension.swift
+++ Sources/Courier/Service/CourierNotificationServiceExtension.swift
@@ -13,24 +13,21 @@ open class CourierNotificationServiceExtension: UNNotificationServiceExtension {
     var contentHandler: ((UNNotificationContent) -> Void)?
     var bestAttemptContent: UNMutableNotificationContent?
 
-    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+    open override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
         self.contentHandler = contentHandler
         bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
         
         if let bestAttemptContent = bestAttemptContent {
-            // Modify the notification content here...
             
-            if let deviceId = UIDevice.current.identifierForVendor?.uuidString {
-                bestAttemptContent.title = "\(bestAttemptContent.title) [\(deviceId)]"
-            } else {
-                bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
-            }
+            let userId = Courier.shared.userId ?? "Missing"
+            bestAttemptContent.title = "\(bestAttemptContent.title) [\(userId)]"
             
             contentHandler(bestAttemptContent)
+            
         }
     }
     
-    override func serviceExtensionTimeWillExpire() {
+    open override func serviceExtensionTimeWillExpire() {
         // Called just before the extension will be terminated by the system.
         // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
         if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
