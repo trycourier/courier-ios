6ec4db7fb9191cb19caefc14e0ae0893e733ca5b
Michael Miller <mike@Michaels-MacBook-Pro.local>
2022-08-12 12:50:50 -0400

    Updated Examples

--- Examples/Swift+Storyboard+FCM/Swift+Storyboard+FCM/AppDelegate.swift
+++ Examples/Swift+Storyboard+FCM/Swift+Storyboard+FCM/AppDelegate.swift
@@ -11,20 +11,80 @@ import FirebaseMessaging
 import Courier
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+class AppDelegate: CourierDelegate {
+    
+    private func configureFirebase() {
         
-        // Init Firebase
-        
+        // Configure firebase programatically
+        // You can also do this with the GoogleService-Info.plist file
+        let options = FirebaseOptions(
+            googleAppID: "<FIREBASE_GOOGLE_APP_ID>",
+            gcmSenderID: "<FIREBASE_GCM_SENDER_ID>"
+        )
+        options.projectID = "<FIREBASE_PROJECT_ID>"
+        options.apiKey = "<FIREBASE_API_KEY>"
         
         FirebaseApp.configure(options: options)
         
-//        if let token = Courier.shared.rawApnsToken {
-//            Messaging.messaging().setAPNSToken(token, type: .sandbox)
-//        }
+        // Register the messaging delegate
+        Messaging.messaging().delegate = self
         
+    }
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        configureFirebase()
         return true
+        
+    }
+    
+    // If you extend the CourierDelegate, the Courier SDK will
+    // automatically manage the APNS token and track notification status analytics
+    
+    override func pushNotificationDeliveredInForeground(message: [AnyHashable : Any]) -> UNNotificationPresentationOptions {
+        
+        print("\n=== 💌 Push Notification Delivered In Foreground ===\n")
+        print(message)
+        print("\n=================================================\n")
+        
+        // This is how you want to show your notification in the foreground
+        // You can pass "[]" to not show the notification to the user or
+        // handle this with your own custom styles
+        return [.sound, .list, .banner, .badge]
+        
+    }
+    
+    override func pushNotificationClicked(message: [AnyHashable : Any]) {
+        
+        print("\n=== 👉 Push Notification Clicked ===\n")
+        print(message)
+        print("\n=================================\n")
+        
+    }
+    
+    override func deviceTokenDidChange(rawApnsToken: Data, isDebugging: Bool) {
+        
+        // Sync the current apns token with firebase
+        // This will trigger other important FCM functions to get called
+        // Be sure to handle the type properly here in your production app
+        Messaging.messaging().setAPNSToken(rawApnsToken, type: isDebugging ? .sandbox : .prod)
+        
+    }
+
+}
+
+extension AppDelegate: MessagingDelegate {
+  
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        Task {
+            do {
+                if let token = fcmToken {
+                    try await Courier.shared.setFCMToken(token)
+                }
+            } catch {
+                print(error)
+            }
+        }
     }
 
 }
--- Examples/Swift+Storyboard+FCM/Swift+Storyboard+FCM/ViewController.swift
+++ Examples/Swift+Storyboard+FCM/Swift+Storyboard+FCM/ViewController.swift
@@ -6,14 +6,75 @@
 //
 
 import UIKit
+import Courier
 
 class ViewController: UIViewController {
+    
+    private let accessToken = "pk_prod_X9SHD669JF400NHY56KYPTE639HH"
+    private let userId = "example_user"
 
+    @IBAction func sendPush(_ sender: Any) {
+        
+        Task {
+            
+            do {
+                
+                try await Courier.sendPush(
+                    authKey: accessToken,
+                    userId: userId,
+                    title: "Test Push Notification",
+                    message: "Hello from Courier! 🐣",
+                    providers: [.fcm]
+                )
+                
+            } catch {
+                print(error)
+            }
+            
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        Task {
+            
+            do {
+                
+                // To hide debugging logs
+//                Courier.shared.isDebugging = false
+                
+                // Set the access token to your user id
+                // You can use a Courier auth key for this
+                // but it is recommended that use use a jwt linked to your user
+                // More info: https://www.courier.com/docs/reference/auth/issue-token/
+                
+                // This should be synced with your user's state management to ensure
+                // your users tokens don't receive notifications when they are not
+                // authenticated to use your app
+                try await Courier.shared.setCredentials(
+                    accessToken: accessToken,
+                    userId: userId
+                )
+                
+                // You should requests this permission in a place that
+                // makes most sense for your user's experience
+                try await Courier.requestNotificationPermissions()
+                
+                // To remove the tokens for the current user, call this function.
+                // You should call this when your user signs out of your app
+//                try await Courier.shared.signOut()
+                
+            } catch {
+                
+                print(error)
+                
+            }
+            
+        }
+        
     }
-
 
 }
 
