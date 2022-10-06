3eb117c01a9c686b77aff6a61d2f31d9658356f1
Michael Miller <mike@Michaels-MacBook-Pro-2.local>
2022-10-06 17:27:14 -0400

    Test

---
+++ Sources/Courier/TestSwift.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Michael Miller on 10/6/22.
+//
+
+import Foundation
+
+@objc(TestSwift) open class TestSwift: NSObject {
+    
+    @objc func hello() {
+        print("Hello!")
+    }
+    
+}
