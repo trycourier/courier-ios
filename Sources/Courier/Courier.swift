16c71adc5c81e5faf90d2eed3347bae4d88d0ec1
Michael Miller <mike@Michaels-MacBook-Pro-2.local>
2022-10-13 15:25:56 -0400

    Better enum support

--- Sources/Courier/Courier.swift
+++ Sources/Courier/Courier.swift
@@ -20,7 +20,7 @@ import UIKit
      */
     
     public static var agent = CourierAgent.native_ios
-    internal static let version = "1.0.15"
+    internal static let version = "1.0.16"
     
     // MARK: Init
     
