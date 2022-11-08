a4b04a90fb2789ef057ff2fded1fae575df52d35
FahadAminShovon <29832989+FahadAminShovon@users.noreply.github.com>
2022-11-09 02:13:15 +0600

    feat(env): read value from Env.swift file SwiftUIAPNS

---
+++ Examples/SwiftUIAPNS/SwiftUIAPNS/EnvSample.swift
@@ -0,0 +1,14 @@
+//
+//  Env.swift
+//  SwiftStoryboardAPNS
+//
+//  Created by Fahad Amin on 9/11/22.
+//
+
+import Foundation
+
+class SampleEnv {
+    static var COURIER_ACCESS_TOKEN = "XXXXX"
+    static var COURIER_AUTH_KEY="XXXXX"
+    static var COURIER_USER_ID="XXXXX"
+}
