2fd80091c65f0b9530fc666eba77ef92b3ad2864
mikemilla <mike@mikemiller.design>
2022-11-10 15:13:11 -0500

    Env script polish

---
+++ env-setup.sh
@@ -0,0 +1,17 @@
+# ENV variables
+APP_FILE=Examples/SwiftStoryboardAPNS/SwiftStoryboardAPNS/Env.swift
+TEST_FILE=Tests/CourierTests/Env.swift
+
+# Create the app file env
+if [[ ! -e $APP_FILE ]]
+then
+  cp EnvSample.swift $APP_FILE
+fi
+
+# Create the androidTest env
+if [[ ! -e $TEST_FILE ]]
+then
+  cp EnvSample.swift $TEST_FILE
+fi
+
+echo "🙌 Env files created"
