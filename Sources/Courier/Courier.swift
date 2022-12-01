4c10b61cf64bd9899a7c5c4c8fde2e4e66c10098
mikemilla <mike@mikemiller.design>
2022-12-01 11:34:04 -0500

    Bump

--- Sources/Courier/Courier.swift
+++ Sources/Courier/Courier.swift
@@ -20,7 +20,7 @@ import UIKit
      */
     
     public static var agent = CourierAgent.native_ios
-    internal static let version = "1.0.21"
+    internal static let version = "1.1.0"
     
     // MARK: Init
     
