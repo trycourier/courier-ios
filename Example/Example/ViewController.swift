7875b8c3371f81f9f5c2ada58c16727859b509bf
FahadAminShovon <29832989+FahadAminShovon@users.noreply.github.com>
2022-12-09 23:14:50 +0600

    add userId alert

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -27,14 +27,55 @@ class ViewController: UIViewController {
                 
             } else {
                 
-                try await Courier.shared.signIn(
-                    accessToken: Env.COURIER_ACCESS_TOKEN,
-                    userId: Env.COURIER_USER_ID
+                let alert = UIAlertController(
+                    title: "Sign In with Courier User Id",
+                    message: "Please enter Courier User Id to continue.",
+                    preferredStyle: .alert
                 )
+                present(alert, animated: true)
                 
-                refresh()
+                alert.addTextField{ field in
+                    field.placeholder = "Courier User Id"
+                    field.keyboardType = .default
+                    field.autocorrectionType = .no
+                    field.autocapitalizationType = .none
+                    field.returnKeyType = .continue
+                }
                 
-                try await Courier.requestNotificationPermission()
+                alert.addAction(UIAlertAction(
+                    title: "Cancel",
+                    style: .cancel,
+                    handler: nil
+                ))
+                
+                alert.addAction(UIAlertAction(
+                    title: "Sign In",
+                    style: .default,
+                    handler: {_ in
+                        
+                        guard let fields = alert.textFields, fields.count == 1 else {
+                            return
+                        }
+                        let userIdField = fields[0]
+                        
+                        guard let userId = userIdField.text, !userId.isEmpty else{
+                           return
+                        }
+                        
+                        Task{
+                            try await Courier.shared.signIn(
+                                accessToken: Env.COURIER_ACCESS_TOKEN,
+                                userId: userId
+                            )
+                            
+                            self.refresh()
+                        
+                            try await Courier.requestNotificationPermission()
+                        }
+                       
+                    }
+                ))
+
                 
             }
             
@@ -58,15 +99,19 @@ class ViewController: UIViewController {
             
             let messageProviders = providers.map { $0.rawValue }.joined(separator: " and ")
             
-            if (!providers.isEmpty) {
-                try await Courier.shared.sendPush(
-                    authKey: Env.COURIER_AUTH_KEY,
-                    userId: Env.COURIER_USER_ID,
-                    title: "Hey \(Env.COURIER_USER_ID)!",
-                    message: "This is a test push sent through \(messageProviders)",
-                    isProduction: false,
-                    providers: providers
-                )
+            if let userId = Courier.shared.userId {
+                
+                if (!providers.isEmpty) {
+                    try await Courier.shared.sendPush(
+                        authKey: Env.COURIER_AUTH_KEY,
+                        userId: userId,
+                        title: "Hey \(userId)!",
+                        message: "This is a test push sent through \(messageProviders)",
+                        isProduction: false,
+                        providers: providers
+                    )
+                }
+                
             }
             
         }
