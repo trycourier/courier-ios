5f98c48e991ee13eb9e77b2a76c1037f13209203
mikemilla <mike@mikemiller.design>
2023-03-07 13:37:38 -0500

    Page trigger

--- Sources/Courier/Views/CourierInbox.swift
+++ Sources/Courier/Views/CourierInbox.swift
@@ -207,9 +207,9 @@ import UIKit
     }
     
     public func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-        if (indexPath.section == 1) {
-            Courier.shared.fetchNextPageOfMessages()
-        }
+//        if (indexPath.section == 1) {
+//            Courier.shared.fetchNextPageOfMessages()
+//        }
     }
     
     public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
@@ -223,17 +223,17 @@ import UIKit
         // Call delegate
         delegate?.didScrollInbox?(scrollView: scrollView)
         
-//        // Get positions
-//        let safeAreaHeight = scrollView.safeAreaInsets.top + scrollView.safeAreaInsets.bottom
-//        let viewHeight = scrollView.bounds.height - safeAreaHeight
-//        let scrollY = scrollView.contentOffset.y + scrollView.safeAreaInsets.top
-//        let distanceToBottom = scrollY + viewHeight
-//        let pageCalc = abs(distanceToBottom - scrollView.contentSize.height)
-//
-//        // Handle pagination
-//        if (pageCalc < getPaginationTrigger()) {
-//            Courier.shared.fetchNextPageOfMessages()
-//        }
+        // Get positions
+        let safeAreaHeight = scrollView.safeAreaInsets.top + scrollView.safeAreaInsets.bottom
+        let viewHeight = scrollView.bounds.height - safeAreaHeight
+        let scrollY = scrollView.contentOffset.y + scrollView.safeAreaInsets.top
+        let distanceToBottom = scrollY + viewHeight
+        let pageCalc = abs(distanceToBottom - scrollView.contentSize.height)
+
+        // Handle pagination
+        if (pageCalc < getPaginationTrigger()) {
+            Courier.shared.fetchNextPageOfMessages()
+        }
         
     }
     
