87946ec6d72a3d1ca23a5d6964c977a039f98bd9
mikemilla <mike@mikemiller.design>
2023-03-07 12:59:43 -0500

    Paging fix

--- Sources/Courier/Modules/CoreInbox.swift
+++ Sources/Courier/Modules/CoreInbox.swift
@@ -489,11 +489,6 @@ internal class CoreInbox {
     
     internal func fetchNextPage() {
         
-        let canPaginate = inboxData?.messages?.pageInfo?.hasNextPage ?? false
-        if (!canPaginate) {
-            return
-        }
-        
         fetch?.cancel()
         
         fetch = Task {
