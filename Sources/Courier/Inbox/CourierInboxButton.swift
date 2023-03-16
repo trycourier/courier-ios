a0c6905a95485bd143ea768e4870b66261cde3c1
mikemilla <mike@mikemiller.design>
2023-03-16 11:25:02 -0400

    Test

--- Sources/Courier/Inbox/CourierInboxButton.swift
+++ Sources/Courier/Inbox/CourierInboxButton.swift
@@ -12,8 +12,8 @@ internal class CourierInboxButton: UIButton {
     internal func setTheme(_ theme: CourierInboxTheme) {
         
         contentEdgeInsets = UIEdgeInsets(top: 0, left: CourierInboxTheme.margin, bottom: 0, right: CourierInboxTheme.margin)
-        titleLabel?.font = theme.buttonStyles.font?.font
-        titleLabel?.textColor = theme.buttonStyles.font?.color
+        titleLabel?.font = theme.buttonStyles.font.font
+        titleLabel?.textColor = theme.buttonStyles.font.color
         backgroundColor = theme.buttonStyles.backgroundColor
         layer.cornerRadius = theme.buttonStyles.cornerRadius
         
@@ -42,6 +42,27 @@ internal class CourierInboxButton: UIButton {
 //
 //        }
         
+    }
+    
+    private func animate(fadeIn: Bool) {
+        UIView.animate(withDuration: 0.6, animations: {
+            self.alpha = fadeIn ? 1 : 0.5
+        })
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        animate(fadeIn: false)
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesCancelled(touches, with: event)
+        animate(fadeIn: true)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        animate(fadeIn: true)
     }
 
 }
