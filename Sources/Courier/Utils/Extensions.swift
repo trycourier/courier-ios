ab5a4bb80817e13f04167ad98034f4f524f31b6c
mikemilla <mike@mikemiller.design>
2023-03-17 15:56:31 -0400

    Color update

--- Sources/Courier/Utils/Extensions.swift
+++ Sources/Courier/Utils/Extensions.swift
@@ -110,55 +110,30 @@ extension Date {
 }
 
 extension UIColor {
+  
+  internal convenience init(_ hex: String, alpha: CGFloat = 1.0) {
+      
+      var cString = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
     
-    public convenience init?(hex: String) {
-        
-        let r, g, b, a: CGFloat
+      if cString.hasPrefix("#") {
+          cString.removeFirst()
+      }
+    
+      if cString.count != 6 {
+          self.init("ff0000") // return red color for wrong hex input
+          return
+      }
+    
+      var rgbValue: UInt64 = 0
+      Scanner(string: cString).scanHexInt64(&rgbValue)
+    
+      self.init(
+        red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+        green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+        blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+        alpha: alpha
+      )
+      
+  }
 
-        if hex.hasPrefix("#") {
-            
-            let start = hex.index(hex.startIndex, offsetBy: 1)
-            let hexColor = String(hex[start...])
-
-            if hexColor.count == 8 {
-                
-                let scanner = Scanner(string: hexColor)
-                var hexNumber: UInt64 = 0
-
-                if scanner.scanHexInt64(&hexNumber) {
-                    
-                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
-                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
-                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
-                    a = CGFloat(hexNumber & 0x000000ff) / 255
-
-                    self.init(red: r, green: g, blue: b, alpha: a)
-                    return
-                }
-                
-            }
-            
-            if hexColor.count == 6 {
-                
-                let scanner = Scanner(string: hexColor)
-                var hexNumber: UInt64 = 0
-
-                if scanner.scanHexInt64(&hexNumber) {
-                    
-                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
-                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
-                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
-                    a = 255
-
-                    self.init(red: r, green: g, blue: b, alpha: a)
-                    return
-                }
-                
-            }
-            
-        }
-
-        return nil
-        
-    }
 }
