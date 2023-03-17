9b9b2fc8845991eaa3d19ca2226afc109d8e2746
mikemilla <mike@mikemiller.design>
2023-03-17 15:58:44 -0400

    Colors

--- Sources/Courier/Utils/Extensions.swift
+++ Sources/Courier/Utils/Extensions.swift
@@ -111,29 +111,28 @@ extension Date {
 
 extension UIColor {
   
-  internal convenience init(_ hex: String, alpha: CGFloat = 1.0) {
+    internal convenience init?(_ hex: String, alpha: CGFloat = 1.0) {
       
-      var cString = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        var cString = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
     
-      if cString.hasPrefix("#") {
-          cString.removeFirst()
-      }
+        if cString.hasPrefix("#") {
+            cString.removeFirst()
+        }
     
-      if cString.count != 6 {
-          self.init("ff0000") // return red color for wrong hex input
-          return
-      }
+        if cString.count != 6 {
+            return nil
+        }
     
-      var rgbValue: UInt64 = 0
-      Scanner(string: cString).scanHexInt64(&rgbValue)
+        var rgbValue: UInt64 = 0
+        Scanner(string: cString).scanHexInt64(&rgbValue)
     
-      self.init(
-        red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
-        green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
-        blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
-        alpha: alpha
-      )
+        self.init(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: alpha
+        )
       
-  }
+    }
 
 }
