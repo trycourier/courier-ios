fd6c176078262036e1e79e3962bc752978e338fa
mikemilla <mike@mikemiller.design>
2023-03-23 14:09:01 -0400

    Bundle

--- Sources/Courier/Utils/Extensions.swift
+++ Sources/Courier/Utils/Extensions.swift
@@ -186,7 +186,7 @@ extension Bundle {
             #if SWIFT_PACKAGE
                 return Bundle.module
             #else
-                return Bundle.main
+                return Bundle(for: Self.self)
             #endif
         }
     }
