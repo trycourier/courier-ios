08d0b48800ac1ddc3568c9391f0d0cbd46be2c27
mikemilla <mike@mikemiller.design>
2023-03-27 12:47:59 -0400

    Removed pods

--- Pod-Example/Pods/Target Support Files/Courier_iOS/Courier_iOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Courier_iOS : NSObject
-@end
-@implementation PodsDummy_Courier_iOS
-@end
--- Pod-Example/Pods/Target Support Files/Pods-CourierService/Pods-CourierService-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CourierService : NSObject
-@end
-@implementation PodsDummy_Pods_CourierService
-@end
