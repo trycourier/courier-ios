c5464698d7b697420c78aca710d79ff56fb850f8
mikemilla <mike@mikemiller.design>
2023-04-17 17:16:06 -0400

    Test

--- Sources/Courier_iOS/Models/Public/InboxAction.swift
+++ Sources/Courier_iOS/Models/Public/InboxAction.swift
@@ -9,50 +9,20 @@ import Foundation
 
 @objc public class InboxAction: NSObject, Codable {
     
+    // TODO: Decode data by hand...
+    
     // MARK: Properties
     
     @objc public let content: String?
     @objc public let href: String?
     @objc public let style: String?
     @objc public let backgroundColor: String?
-
-    private var jsonData: String?
     
-    @objc public var data: [String: Any]? {
-        get {
-            
-            if let data = jsonData?.data(using: .utf8) {
-                do {
-                    if let dictionary = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
-                        return dictionary
-                    }
-                } catch {
-                    print("Error converting JSON string to dictionary: \(error.localizedDescription)")
-                }
-            }
-            
-            return nil
-            
-        }
-    }
-    
-    public init(content: String?, href: String?, style: String?, backgroundColor: String?, data: [String: Any]?) {
-        
+    public init(content: String?, href: String?, style: String?, backgroundColor: String?) {
         self.content = content
         self.href = href
         self.style = style
         self.backgroundColor = backgroundColor
-        
-        // Get JSON string from data
-        if let data = data {
-            do {
-                let json = try JSONSerialization.data(withJSONObject: data, options: [])
-                self.jsonData = String(data: json, encoding: .utf8)
-            } catch {
-                print("Error converting dictionary to string: \(error.localizedDescription)")
-            }
-        }
-        
     }
     
     enum CodingKeys: String, CodingKey {
@@ -60,7 +30,6 @@ import Foundation
         case href
         case style
         case backgroundColor = "background_color"
-        case jsonData = "data"
     }
     
 }
