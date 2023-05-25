7aa06dfd7336bc193ce613d49a7981562a62d449
mikemilla <mike@mikemiller.design>
2023-05-25 12:28:45 -0700

    Release

--- Sources/Courier_iOS/Courier_iOS.swift
+++ Sources/Courier_iOS/Courier_iOS.swift
@@ -29,7 +29,7 @@ import Foundation
      * Versioning
      */
     public static var agent = CourierAgent.native_ios
-    internal static let version = "2.0.11"
+    internal static let version = "2.0.2"
     
     /**
      * Singleton reference to the SDK
