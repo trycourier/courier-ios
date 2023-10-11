1a813c41f70fdeaf633b27f87d69df00eedb5774
mikemilla <mike@mikemiller.design>
2023-10-11 10:36:33 -0700

    Example app update

--- Tests/CourierTests/ExampleServer.swift
+++ Tests/CourierTests/ExampleServer.swift
@@ -8,43 +8,8 @@
 import Foundation
 
 class ExampleServer {
-
-    private struct Response: Codable {
-        let token: String
-    }
-
-    internal func generateJwt(authKey: String, userId: String) async throws -> String {
-
-        return try await withCheckedThrowingContinuation({ (continuation: CheckedContinuation<String, Error>) in
-
-            let url = URL(string: "https://api.courier.com/auth/issue-token")!
-            var request = URLRequest(url: url)
-            request.httpMethod = "POST"
-            
-            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-            request.addValue("Bearer \(authKey)", forHTTPHeaderField: "Authorization")
-            
-            request.httpBody = try? JSONEncoder().encode([
-                "scope": "user_id:\(userId) write:user-tokens write:preferences read:preferences",
-                "expires_in": "2 days"
-            ])
-
-            let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-                do {
-                    let res = try JSONDecoder().decode(Response.self, from: data ?? Data())
-                    continuation.resume(returning: res.token)
-                } catch {
-                    continuation.resume(throwing: error)
-                }
-            }
-
-            task.resume()
-
-        })
-
-    }
     
-    internal func sendTest(authKey: String, userId: String, key: String) async throws -> Data? {
+    internal func sendTest(authKey: String, userId: String, providers: [String], title: String, body: String) async throws -> Data? {
         
         return try await withCheckedThrowingContinuation({ (continuation: CheckedContinuation<Data?, Error>) in
             
@@ -61,12 +26,12 @@ class ExampleServer {
                         "user_id": userId
                     ],
                     "content": [
-                        "title": "Test",
-                        "body": "Body"
+                        "title": title,
+                        "body": body
                     ],
                     "routing": [
                         "method": "all",
-                        "channels": [key]
+                        "channels": providers
                     ]
                 ]
             ].toJson()
