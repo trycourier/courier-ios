6c3d525ca845adce237367de4649185573d4dc67
mikemilla <mike@mikemiller.design>
2024-04-05 11:59:41 -0600

    JWT in demo

--- Example/Example/Auth/AuthViewController.swift
+++ Example/Example/Auth/AuthViewController.swift
@@ -18,14 +18,17 @@ class AuthViewController: UIViewController {
         ShowTime.enabled = showTouchesSwitch.isOn ? .always : .never
     }
     
+    private var authListener: CourierAuthenticationListener? = nil
+    
     @IBAction func authButtonAction(_ sender: Any) {
         
         if let _ = Courier.shared.userId {
             
             Task {
+                
+                self.authButton.isEnabled = false
             
                 try await Courier.shared.signOut()
-                refresh()
                 
             }
             
@@ -34,6 +37,8 @@ class AuthViewController: UIViewController {
             showInputAlert(title: "Sign in", placeHolder: "Enter Courier User Id", action: "Sign In") { userId in
                 
                 Task {
+                    
+                    self.authButton.isEnabled = false
                     
                     let jwt = try await ExampleServer().generateJwt(
                         authKey: Env.COURIER_AUTH_KEY,
@@ -44,8 +49,6 @@ class AuthViewController: UIViewController {
                         accessToken: jwt,
                         userId: userId
                     )
-                    
-                    self.refresh()
                     
                 }
                 
@@ -63,24 +66,56 @@ class AuthViewController: UIViewController {
         ShowTime.enabled = .never
         showTouchesSwitch.setOn(ShowTime.enabled == .always, animated: false)
 
-        refresh()
-        
-    }
-    
-    private func refresh() {
-        
-        if let userId = Courier.shared.userId {
+        Task {
             
-            authButton.setTitle("Sign Out", for: .normal)
-            authLabel.text = "Courier User Id: \(userId)"
+            if let userId = Courier.shared.userId {
+                
+                self.authButton.isEnabled = false
+                self.authLabel.text = "Refreshing JWT..."
+                
+                // Remove the existing user
+                try await Courier.shared.signOut()
+                
+                // Get the JWT
+                let jwt = try await ExampleServer().generateJwt(
+                    authKey: Env.COURIER_AUTH_KEY,
+                    userId: userId
+                )
+                
+                // Sign in with JWT
+                try await Courier.shared.signIn(
+                    accessToken: jwt,
+                    userId: userId
+                )
+                
+                self.refresh(userId)
+                
+            }
             
-        } else {
-            
-            authButton.setTitle("Sign In", for: .normal)
-            authLabel.text = "No Courier User Id Found"
+            authListener = Courier.shared.addAuthenticationListener { [weak self] userId in
+                self?.refresh(userId)
+            }
             
         }
         
+    }
+    
+    private func refresh(_ userId: String?) {
+        
+        if let userId = userId {
+            authButton.setTitle("Sign Out", for: .normal)
+            authLabel.text = "Courier User Id: \(userId)"
+        } else {
+            authButton.setTitle("Sign In", for: .normal)
+            authLabel.text = "No Courier User Id Found"
+        }
+        
+        authButton.isEnabled = true
+        
+    }
+    
+    deinit {
+        authListener?.remove()
     }
 
 }
