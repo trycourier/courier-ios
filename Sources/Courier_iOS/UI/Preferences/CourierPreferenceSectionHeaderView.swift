ee71dbe8bae73d39ae46b2f7131cfde24d360075
mikemilla <mike@mikemiller.design>
2024-04-10 10:57:41 -0600

    Release

--- Sources/Courier_iOS/UI/Preferences/CourierPreferenceSectionHeaderView.swift
+++ Sources/Courier_iOS/UI/Preferences/CourierPreferenceSectionHeaderView.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+@available(iOSApplicationExtension, unavailable)
 class CourierPreferenceSectionHeaderView: UITableViewHeaderFooterView {
     
     static let id = "CourierPreferenceSectionHeaderView"
