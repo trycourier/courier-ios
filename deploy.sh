9dcd5e921dc71e06c436c3a7605c580a098d8d8f
mikemilla <mike@mikemiller.design>
2024-06-21 16:13:50 -0400

    🚀 3.5.5

--- deploy.sh
+++ deploy.sh
@@ -14,8 +14,9 @@ declare -a steps=(
     "Run Tests:sh run_tests.sh"
     "Build Demo App:sh build_demo_app.sh"
     "Update Build Version:sh update_version.sh"
+    "Install Brew:sh install_homebrew.sh"
     "Create Git Release:sh git_release.sh"
-    "Distribute Release:sh distribute_release.sh"
+    "Release Cocoapod:sh release_pod.sh"
 )
 
 # Display available steps with indices
