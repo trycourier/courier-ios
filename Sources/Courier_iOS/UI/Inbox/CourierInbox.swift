1e84bc1c14c2703f27177d54dac1e05950825b98
mikemilla <mike@mikemiller.design>
2024-09-26 10:52:21 -0700

    tab

--- Sources/Courier_iOS/UI/Inbox/CourierInbox.swift
+++ Sources/Courier_iOS/UI/Inbox/CourierInbox.swift
@@ -224,9 +224,17 @@ open class CourierInbox: UIView, UIScrollViewDelegate, UIGestureRecognizerDelega
     }
     
     private func updateScrollViewToPage(_ index: Int) {
+        // Disable scrolling while the page change is happening
+        scrollView.isScrollEnabled = false
+        
         let pageWidth = scrollView.frame.size.width
         let offset = CGPoint(x: pageWidth * CGFloat(index), y: 0)
         scrollView.setContentOffset(offset, animated: true)
+        
+        // Re-enable scrolling after the animation is complete
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) { // Adjust delay as needed
+            self.scrollView.isScrollEnabled = true
+        }
     }
     
     private func toggleCourierBar(brand: CourierBrand?) {
