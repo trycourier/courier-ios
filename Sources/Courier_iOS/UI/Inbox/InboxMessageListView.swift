2f014b910991e61f0f0480c44c3e3abe86a7e766
mikemilla <mike@mikemiller.design>
2024-10-03 17:17:21 -0700

    Test

--- Sources/Courier_iOS/UI/Inbox/InboxMessageListView.swift
+++ Sources/Courier_iOS/UI/Inbox/InboxMessageListView.swift
@@ -193,7 +193,7 @@ internal class InboxMessageListView: UIView, UITableViewDelegate, UITableViewDat
         self.inboxMessages[index] = message
         self.state = inboxMessages.isEmpty ? .empty : .content
         let indexPath = IndexPath(row: index, section: 0)
-        self.tableView.reloadRows(at: [indexPath], with: theme.messageAnimationStyle)
+        self.tableView.reloadRows(at: [indexPath], with: .automatic)
         self.openVisibleMessages()
     }
     
