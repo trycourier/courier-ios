41f1b2bfc7cedf2676cf00cdeed1ddbdc875e33d
mikemilla <mike@mikemiller.design>
2024-12-09 11:34:47 -0500

    🚀 5.3.5

--- Sources/Courier_iOS/Courier_iOS.swift
+++ Sources/Courier_iOS/Courier_iOS.swift
@@ -29,7 +29,7 @@ import UIKit
     /**
      * Versioning
      */
-    internal static let version = "5.3.4"
+    internal static let version = "5.3.5"
     @objc public static var agent = CourierAgent.nativeIOS(version)
     
     /**
