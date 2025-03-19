f9839d535f5b82734d70788e75f44f704a07a878
Mike Miller <mike@mikemiller.design>
2025-03-19 11:58:22 -0600

    Tenant Test

--- Tests/CourierTests/ExampleServer.swift
+++ Tests/CourierTests/ExampleServer.swift
@@ -34,7 +34,7 @@ class ExampleServer {
         
     }
     
-    static func sendTest(authKey: String, userId: String, channel: String) async throws -> String {
+    static func sendTest(authKey: String, userId: String, tenantId: String? = nil, channel: String) async throws -> String {
         
         return try await withCheckedThrowingContinuation({ (continuation: CheckedContinuation<String, Error>) in
             
@@ -45,11 +45,17 @@ class ExampleServer {
             request.addValue("application/json", forHTTPHeaderField: "Content-Type")
             request.addValue("Bearer \(authKey)", forHTTPHeaderField: "Authorization")
             
+            let toField: [String: Any] = {
+                if let tenantId = tenantId {
+                    return ["tenant_id": tenantId]
+                } else {
+                    return ["user_id": userId]
+                }
+            }()
+            
             request.httpBody = [
                 "message": [
-                    "to": [
-                        "user_id": userId
-                    ],
+                    "to": toField,
                     "content": [
                         "title": "Test",
                         "body": "Body"
@@ -151,76 +157,6 @@ class ExampleServer {
                                 "optional": false,
                             ],
                         ]
-                    ]
-                ]
-            ].toJson()
-            
-            let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-                let json = try? JSONSerialization.jsonObject(with: data ?? Data(), options: []) as? [String: Any]
-                let requestId = json?["requestId"] as? String ?? "Error"
-                continuation.resume(returning: requestId)
-            }
-            
-            task.resume()
-            
-        })
-        
-    }
-
-    internal func generateJwt(authKey: String, userId: String) async throws -> String {
-
-        return try await withCheckedThrowingContinuation({ (continuation: CheckedContinuation<String, Error>) in
-
-            let url = URL(string: "https://api.courier.com/auth/issue-token")!
-            var request = URLRequest(url: url)
-            request.httpMethod = "POST"
-            
-            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-            request.addValue("Bearer \(authKey)", forHTTPHeaderField: "Authorization")
-            
-            request.httpBody = try? JSONEncoder().encode([
-                "scope": "user_id:\(userId) write:user-tokens inbox:read:messages inbox:write:events read:preferences write:preferences read:brands",
-                "expires_in": "365 days"
-            ])
-
-            let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-                do {
-                    let res = try JSONDecoder().decode(Response.self, from: data ?? Data())
-                    continuation.resume(returning: res.token)
-                } catch {
-                    continuation.resume(throwing: error)
-                }
-            }
-
-            task.resume()
-
-        })
-
-    }
-    
-    internal func sendTest(authKey: String, userId: String, key: String) async throws -> String {
-        
-        return try await withCheckedThrowingContinuation({ (continuation: CheckedContinuation<String, Error>) in
-            
-            let url = URL(string: "https://api.courier.com/send")!
-            var request = URLRequest(url: url)
-            request.httpMethod = "POST"
-            
-            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-            request.addValue("Bearer \(authKey)", forHTTPHeaderField: "Authorization")
-            
-            request.httpBody = [
-                "message": [
-                    "to": [
-                        "user_id": userId
-                    ],
-                    "content": [
-                        "title": "Test",
-                        "body": "Body"
-                    ],
-                    "routing": [
-                        "method": "all",
-                        "channels": [key]
                     ]
                 ]
             ].toJson()
